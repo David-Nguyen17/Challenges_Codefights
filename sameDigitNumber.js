4f95f22a632e2e25236b0ab579cccee4947fb85e
VanDu97 <nguyenvandu199740@gmail.com>
2019-03-23 08:21:47 +0700

    sameDigitNumber-2

--- sameDigitNumber.js
+++ sameDigitNumber.js
@@ -2,12 +2,12 @@ sameDigitNumber = n => {
   var s = 0;
   while (n > 0) {
     s = n % 10;
-    if (s != n % 10) {
+    n = Math.floor(n / 10);
+    if ((s != n % 10) & (n > 0)) {
       return false;
     }
-    n = Math.floor(n / 10);
   }
   return true;
 };
 
-console.log(sameDigitNumber(11111));
+console.log(sameDigitNumber(11112));
